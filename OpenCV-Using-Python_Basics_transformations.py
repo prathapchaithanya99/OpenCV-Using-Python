48e29241e43b6de7cff595e3c5b6414d497b714e
Prathap Chaithanya Somesula <54240436+prathapchaithanya99@users.noreply.github.com>
2023-06-07 15:31:51 +0530

    Add files via upload

---
+++ OpenCV-Using-Python_Basics_transformations.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#pylint:disable=no-member
+
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('Desktop/PRATHAP_PHOTO.jpg')
+cv.imshow('Prathap', img)
+
+# Translation
+def translate(img, x, y):
+    transMat = np.float32([[1,0,x],[0,1,y]])
+    dimensions = (img.shape[1], img.shape[0])
+    return cv.warpAffine(img, transMat, dimensions)
+
+# -x --> Left
+# -y --> Up
+# x --> Right
+# y --> Down
+
+translated = translate(img, -100, 100)
+cv.imshow('Translated', translated)
+
+# Rotation
+def rotate(img, angle, rotPoint=None):
+    (height,width) = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width//2,height//2)
+    
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width,height)
+
+    return cv.warpAffine(img, rotMat, dimensions)
+
+rotated = rotate(img, -45)
+cv.imshow('Rotated', rotated)
+
+rotated_rotated = rotate(img, -90)
+cv.imshow('Rotated Rotated', rotated_rotated)
+
+# Resizing
+resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
+cv.imshow('Resized', resized)
+
+# Flipping
+flip = cv.flip(img, -1)
+cv.imshow('Flip', flip)
+
+cv.waitKey(0)
+
+
+# In[ ]:
+
+
+
+
